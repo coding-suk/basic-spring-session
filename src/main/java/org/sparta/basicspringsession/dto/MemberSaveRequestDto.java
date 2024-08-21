3003b483a02e3e2ad04d203e11d83976833ab9c9
coding-suk <hjldd0523@naver.com>
2024-08-21 17:29:20 +0900

    기본 구조 세팅

---
+++ src/main/java/org/sparta/basicspringsession/dto/MemberSaveRequestDto.java
@@ -0,0 +1,9 @@
+package org.sparta.basicspringsession.dto;
+
+import lombok.Getter;
+
+@Getter
+public class MemberSaveRequestDto {
+
+    private String name;
+}
---
+++ src/main/java/org/sparta/basicspringsession/repository/MemberRepository.java
@@ -0,0 +1,8 @@
+package org.sparta.basicspringsession.repository;
+
+
+import org.sparta.basicspringsession.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
